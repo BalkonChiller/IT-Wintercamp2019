54e77fef7df47f7009bb89621540c99245262813
Duttixxl2 <48014287+Duttixxl2@users.noreply.github.com>
2019-02-26 14:39:30 +0100

    verknüpfung html mit php

--- Passwortvergessen.php
+++ Passwortvergessen.php
@@ -1,10 +1,9 @@
 <?php
-function abschicken()
-{
+
   $betreff="Passwort ändern"
   $nachricht="laber+ link";
   mail($_POST["femail"], $betreff, $nachricht, "From: Absender <IT_Wintercamp@gurke.de>");
-}
+
 
 
 ?>
