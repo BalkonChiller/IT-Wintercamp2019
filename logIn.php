6a8d331888e50b9761cb508a88c521594608357f
JPHelios <johannes.f.peters@gmx.de>
2019-02-27 11:58:34 +0100

    Verbingung zur datenbank steht

---
+++ logIn.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+	</head>
+
+	<body>
+	<div class="main_page">
+		  <div class="page_header floating_element">
+			<span class="floating_element">
+			  Log In - Dumpseite
+			</span>
+		  </div>
+	</div>
+
+<?php
+
+$_db_host = "localhost";
+$_db_datenbank = "wintercamp";
+$_db_benutzername = "root";
+$_db_passwort = "";
+
+session_start();
+$con = mysqli_connect("localhost", "root", "", "wintercamp");
+
+$fbenutzername = $_POST["fbenutzername"];
+$fpasswort1 = $_POST["fpasswort1"];
+
+##################################################################
+
+$sql = "SELECT * FROM nutzer WHERE benutzername = '$fbenutzername'";
+
+$res = mysqli_query($con, $sql);
+
+echo "<table border = 1>";
+
+while ($ausgabe = mysqli_fetch_assoc($res))
+{
+  echo "<tr><td>" . $ausgabe["nID"] . "</td>" .
+     "<td>" . $ausgabe["nachname"] . "</td>" .
+       "<td>" . $ausgabe["vorname"] . "</td></tr>" ;
+
+}
+mysqli_close($con);
+
+echo "</table>";
+?>
+  </body>
+</html>
