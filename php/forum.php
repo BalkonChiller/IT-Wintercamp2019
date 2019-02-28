4b69fb48d334b816f417c76f224d2c056cb165c5
Duttixxl2 <48014287+Duttixxl2@users.noreply.github.com>
2019-02-28 14:48:58 +0100

    Add vorschlag erstellen -PHP & bearbeitung forum - PHP

--- php/forum.php
+++ php/forum.php
@@ -1,62 +1,48 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="homepageCSS.css">
-</head>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
 <body>
-<img src="logo.jpg" width="50%" height="auto">
-<h1>IT-Camp</h1>
-<div class="menu">
-<table>
-    <tr>
-      <th><a href="Homepage.php">Home</a></th>
-      <th>Forum</th>
-      <th>Chat</th>
-      <th>Gallerie</th>
-	  <th>
-	 <?php
-		$isLoggedIn = 0;
-		//$isLoggedIn = $_SESSION["isLoggedIn"];
-		if($isLoggedIn == 1) {
-			echo "Profil";
-		}
-		else {
-			echo "Login";
-		}
-	  ?>
-	  </th>
-    </tr>
-</table>
-</div>
-<br>
-<div class="row">
-   <div class="col-3 col-s-3 menu">
-   </div>
+  <div class="Oben">
+      <h1>Forum</h1>
+  </div>
+  <div class="mitte">
+      <form class="" action="Vorschlag_erstellen.php" method="post">
+          <input type="submit" name="neubeitrag" value="Neuer Vorschlag">
+      </form>
+  <br>
+<?php
+            error_reporting(E_ALL);
 
-    <div class="aside">
-      <h2>Willkommen in Forum WIP</h2>
-	  <!--Dies ist nur ein Dummy für das Design -->
-      <p>
-	  <?php
-echo "<table border>";	           
-echo"<tr>";
-echo"<th>Kategorie</th>";
-echo"<th>letzter Beitrag</th>";
-echo"</tr>";
+            // Zum Aufbau der Verbindung zur Datenbank
+            //$db_link = mysqli_connect ('localhost' , 'root' , '' , 'wintercamp');
+            $db_link = mysqli_connect ('localhost' , 'd02bbde5' , 'Senkel2002' , 'd02bbde5');
+            mysqli_set_charset($db_link, 'utf8');
 
-	  ?>
-	  </p>
+            $wertebeitrag=$db_link->query("SELECT beitragstitel, beschreibung FROM beitrag");
+            //$target = 'zum Vorschlag';    // Die ist die bereits existierende Datei
+
+                  while ($row = $wertebeitrag->fetch_assoc()) {
+                              //ergebniss ausgeben
+                      echo "<div>";
+                      foreach ($row as $field) {
+                          echo $field."<br>";
+                      }
+                      //link($target, $link);
+                      echo "</div><br>";
+
+                      //$link   = 'https://trojato.de/Passwort_aendern.php?hash='.$id; // Dies ist der Dateiname, auf den verlinkt
+
+                  }
+
+?>
+
     </div>
-</div>
-<br>
-<div class="footer">
-<h2 class="widget-title">powered by</h2>
-<br>
-<a href="https://www.communardo.de/"><img src="images/communardo.png" alt="Communardo" height="100px"></a>
-<a href="https://www.sap.com/germany/index.html"><img src="images/sap.png" alt="SAP" height="100px"></a>
-<a href="https://www.t-systems-mms.com/"><img src="images/tsystems.png" alt="T-Systems" height="100px"></a>
-</div>
-</body>
+    <div class="unten">
+
+    </div>
+
+  </body>
 </html>
