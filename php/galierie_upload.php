ab3b77cf574b0a2ad324d9621e9a022a0fc9c76f
hopfenheinz <frederick.kramm@gmail.com>
2019-02-28 15:26:43 +0100

    fertsch
    läuft

--- php/galierie_upload.php
+++ php/galierie_upload.php
@@ -11,6 +11,8 @@
 	$uploadOk = 1;
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 	// Check if image file is a actual image or fake image
+	if ($_SESSION['angemeldet'] == 0)
+	{header("Location: login.html");}
 	if(isset($_POST["submit"])) {
 	    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
 	    if($check !== false) {
@@ -44,6 +46,9 @@
 	} else {
 	    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 	        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+					$gId=1;
+					$sql="INSERT INTO galeriebild (nId, bilderlink, aktivitaet, gId) VALUES ('".$_SESSION['nID']."','".$target_file."' ,0,'".$gId."')";
+					mysqli_query($ok,$sql);
 	    } else {
 	        echo "Sorry, there was an error uploading your file.";
 	    }
