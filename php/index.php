d8beb7c68b0c5ff7a32cdc45e0c56e34d6b6039a
JPHelios <43265991+JPHelios@users.noreply.github.com>
2019-03-19 17:09:36 +0100

    Update index.php
    Include Header und Footer eingeführt. Bitte auf Funktionsfähigkeit überprüfen... ohne Server (oder XAMPP) kann ich die Datei nicht ausführen.

--- php/index.php
+++ php/index.php
@@ -6,50 +6,9 @@
 	</head>
 	
 	<body>
-		<img src="../homepagebilder/images/logox.png" width="100%" height="auto">
-		<div class="menu">
-			<table>
-	    			<tr>
-	     				<th><a href="index.php">Home</a></th>
-	      				<th>Forum</th>
-	      				<th>Chat</th>
-	      				<th>
-						<div class="dropdown">
-							<button class="dropbtn"><b>Galerie</b>
-								<i class="fa fa-caret-down"></i>
-							</button>
-
-	    						<div class="dropdown-content">
-								<a href="#">Winter 2019</a>
-								<a href="#">Sommer 2018</a>
-								<a href="#">Winter 2018</a>
-								<a href="#">Sommer 2017</a>
-								<a href="#">Sommer 2016</a>
-							</div>
-						</div>
-					</th>
-		</div>
-		
-		<th>
-			<?php
-				 if(isset($_SESSION)) {
-
-					if($_SESSION['angemeldet'] != 1) {
-						echo "<a href='../html/logIn.html'> Login </a>";
-					}
-					else {
-						echo "Profil";
-					}
-				 }
-				 else {
-					 echo "<a href='../html/logIn.html'> Login </a>";
-				 }
-				#<a href="logIn.php"> Login </a>if($isLoggedIn == 1)		$fpasswort1 == $passwort
-		  	?>
-	  	</th>
-	    			</tr>
-			</table>
-		</div>
+		<?php
+			include './header.php';
+		?>
 		<br>
 		
 		<div class="row">
