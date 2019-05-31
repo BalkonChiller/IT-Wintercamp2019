2d1235ebb21c823ddb1b88c59bd4aa37d033c302
BalkonChiller <m.wendorff09@gmail.com>
2019-05-31 12:06:43 +0200

    Pfad der Datenbank angepasst

--- galerie/galerie.php
+++ galerie/galerie.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<title>Galerieslider3</title>	
+		<title>Galerieslider3</title>
 <link rel="stylesheet" href="slider3.css">
 <link rel="stylesheet" href="slider3.js">
 <link rel="stylesheet" href="../css/stylesheet1.css">
@@ -34,7 +34,7 @@
 	  <th>
 	 <?php
 	 if(isset($_SESSION)) {
-	
+
 		if($_SESSION['angemeldet'] != 1)
 		{
 			echo "<a href='../html/logIn.html'> Login </a>";
@@ -67,24 +67,24 @@ $benutzer='root';
     /*if (!$ok)
     {
       echo "Fehler bei der Verbindung zu Datenbank";
-    } 
-    else 
+    }
+    else
 	{
 		echo "Verbindung zur Datenbank steht";
 	}*/
 	$id = $_GET['campid'];
-	
+
 	$sqli="SELECT bilderlink,galeriebezeichnung FROM galeriebild,galerie where campId= ".$id." and galerie.gId = galeriebild.campId ";
-	
+
     $r=mysqli_query($ok, $sqli);
-	
+
 	$sTitel = "";
 	while($erg3=mysqli_fetch_array($r, MYSQLI_NUM)) {
-		
+
 		$sTitel = $erg3['1'];
 	}
-	
-	
+
+
     ?>
     <div class="aside">
       <h2><?php echo $sTitel ?></h2>
@@ -93,30 +93,30 @@ $benutzer='root';
 			<div class="inner-wrapper">
 				<ul class="slides">
 					<?php
-	
+
 	$r2=mysqli_query($ok, $sqli);
-	
+
 	if (!empty($r2))
 	{
 		while($erg2=mysqli_fetch_array($r2, MYSQLI_NUM)) {
-			
+
 			echo "<li>";
-			echo '<div class="slide"><img src="bilder/'.$erg2[0].'" ></div>';
+			echo '<div class="slide"><img src="galerie/bilder/'.$erg2[0].'" ></div>';
 			echo "</li>";
 		}
 	}
 	#else {echo " kein Bild in der Datenbank vorhanden";}
 					?>
-				</ul>		
+				</ul>
 			</div>
 		</div>
-	
+
 			<div class="button prev"></div>
 			<div class="button next"></div>
-		
+
 	</div>
-	
-	<script src="jquery_lib.js"></script> 
+
+	<script src="jquery_lib.js"></script>
 	<script src="slider3.js"></script>
     </div>
 </div>
