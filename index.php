82f3fbd9a8c09841f81c3dcf258336bcc4442619
BalkonChiller <m.wendorff09@gmail.com>
2019-05-31 15:33:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,12 @@
 	   		<div class="col-3 col-s-3 menu"></div>
 			<div class="aside">
 	      			<h2>Informationen</h2>
-	      			<p>Du wolltest schon immer einmal in ein IT-Unternehmen schnuppern und Dich ausprobieren? Im Sommer und im Winter hast du die Chance 3 Unternehmen in einer Woche kennenzulernen. Nutze dazu eine Woche deiner Sommer- oder Winterferien und nimm Teil am IT-Camp in Dresden!
+	      			<p>Du wolltest schon immer einmal in ein IT-Unternehmen schnuppern und dich ausprobieren? Im Sommer und im Winter hast du die Chance 3 Unternehmen in einer Woche kennenzulernen. Nutze dazu eine Woche deiner Sommer- oder Winterferien und nimm Teil am IT-Camp in Dresden!
 		  			<br>
 		  			Im Team entwickelt Ihr eine eigene Software und durchlauft alle Positionen der Arbeitskette.
 		  			<br>
-		  			Schicke uns für das Sommercamp ab Mai und für das Wintercamp ab November deine Bewerbung via E-Mail an personal@communardo.de. Für die genauen Infos kannst du unter den Internetseiten <a href="http://it-summercamp-dd.de/">http://it-summercamp-dd.de/</a> und <a href="http://it-wintercamp-dd.de/">http://it-wintercamp-dd.de/</a> mehr herrausfinden.
+		  			Schicke uns für das Sommercamp ab Mai und für das Wintercamp ab November deine Bewerbung via E-Mail an personal@communardo.de. Für die genauen Infos kannst du unter den Internetseiten <a href="http://it-summercamp-dd.de/">http://it-summercamp-dd.de/</a> und <a href="http://it-wintercamp-dd.de/">http://it-wintercamp-dd.de/</a> mehr herausfinden.
+						<br>
 					Nutze die Möglichkeit deinen Traumberuf zu testen!
 		  		</p>
 	    		</div>
