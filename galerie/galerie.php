51fb80e329ee7587f48fbc8bfe20cf730a1be114
crazyKhajiit <becker.bahratal@web.de>
2019-05-31 14:03:20 +0200

    Update galerie.php

--- galerie/galerie.php
+++ galerie/galerie.php
@@ -10,48 +10,11 @@
 <link rel="stylesheet" href="../css/stylesheet1.css">
 </head>
 <body>
-<img src="../homepagebilder/images/logox.png" width="100%" height="auto">
-<div class="menu">
-<table>
-    <tr>
-      <th><a href="index.php">Home</a></th>
-      <th>Forum</th>
-      <th>Chat</th>
-      <th><div class="dropdown">
-		<button class="dropbtn"><b>Galerie</b>
-			<i class="fa fa-caret-down"></i>
-		</button>
+<?php
 
-    <div class="dropdown-content">
-			<a href="?campid=191">Winter 2019</a>
-			<a href="?campid=180">Sommer 2018</a>
-			<a href="?campid=181">Winter 2018</a>
-			<a href="?campid=170">Sommer 2017</a>
-			<a href="?campid=160">Sommer 2016</a>
-	</div>
-		</div>
-		</div></th>
-	  <th>
-	 <?php
-	 if(isset($_SESSION)) {
+include '../php/header.php';
 
-		if($_SESSION['angemeldet'] != 1)
-		{
-			echo "<a href='../html/logIn.html'> Login </a>";
-		}
-		else {
-			echo "Profil";
-		}
-	 }
-	 else {
-		 echo "<a href='../html/logIn.html'> Login </a>";
-	 }
-		#<a href="logIn.php"> Login </a>if($isLoggedIn == 1)		$fpasswort1 == $passwort
-	  ?>
-	  </th>
-    </tr>
-</table>
-</div>
+?>
 <br>
 <div class="row">
    <div class="col-3 col-s-3 menu">
@@ -121,12 +84,10 @@ $benutzer='root';
     </div>
 </div>
 <br>
-<div class="footer">
-<h2 class="widget-title">powered by</h2>
-<br>
-<a href="https://www.communardo.de/"><img src="../homepagebilder/images/communardo.png" srcset="../homepagebilder/images/communardoi.png 320w, ../homepagebilder/images/communardom.png 600w, ../homepagebilder/images/communardo.png 900w" alt="Communardo"></a>
-<a href="https://www.sap.com/germany/index.html"><img src="../homepagebilder/images/sap.png" srcset="../homepagebilder/images/sapi.png 320w, ../homepagebilder/images/sapm.png 600w, ../homepagebilder/images/sap.png 900w" alt="SAP"></a>
-<a href="https://www.t-systems-mms.com/"><img src="../homepagebilder/images/tsystems.png" srcset="../homepagebilder/images/tsystemsi.png 320w,../homepagebilder/images/tsystemsm.png 600w, ../homepagebilder/images/tsystems.png 900w" alt="T-Systems"></a>
-</div>
+<?php
+
+include '../php/footer.php';
+
+?>
 </body>
 </html>
