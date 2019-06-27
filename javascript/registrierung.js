9bf1602fe4fa35da4a334bbefe41cbd465b85f3f
m00rex <48015628+m00rex@users.noreply.github.com>
2019-06-27 23:31:40 +0200

    Überarbeitung Registrierung
    viele fehler gefixt aber noch nicht alles, 1 Fehler verbleibt

--- javascript/registrierung.js
+++ javascript/registrierung.js
@@ -5,85 +5,38 @@ function isValid(str){
 
 function registrieren(){
 var fehler = "";
+
 //passwort
-if (document.getElementsByName("fpasswort1").value != document.getElementsByName("fpasswort2").value){
+if (document.getElementById("fpasswort1").value != document.getElementById("fpasswort2").value){
 fehler += "Die eingegebenen Passwörter stimmen nicht überein\n";
 }
 
+if (document.getElementById("fpasswort1").value.length < 6){
+fehler += "Passwort mit mindestens 6 Zeichen verwenden\n";
+}
+
 //email
-
-if (document.getElementById("femail").value.length < 7){
-fehler += "EMail-Adresse ist zu kurz\n";
-}
-else {
-if (document.getElementById("femail").value.indexOf("@") == -1){
-fehler += "Eine korrekte eMail-Adresse eingeben\n";
-}
-
-else {
-if (document.getElementById("femail").value.indexOf(".", document.getElementById("femail").value.indexOf("@")) == -1){
-fehler += "Nach dem @ muss ein Punkt folgen\n";
-}
-
-}
-
+if (document.getElementById("femail").value.length < 7 || document.getElementById("femail").value.indexOf("@") == -1 || document.getElementById("femail").value.indexOf(".", document.getElementById("femail").value.indexOf("@")) == -1){
+fehler += "Eine korrekte E-Mail-Adresse eingeben\n";
 }
 
 //teilnahmejahr
-var heute=new Date();
+var heute = new Date();
 if (document.getElementById("fcamp").value="SummerCamp"){
-  if (document.getElementById("fjahr").value <2016){
-    fehler += "Ungültiges Jahr eingegeben\n";
-  }
-}
-
-if (document.getElementById("fcamp").value="SummerCamp"){
-  if (document.getElementById("fjahr").value >heute.getFullYear()){
-    fehler+= "Ungültiges Jahr eingegeben\n"
-  }
-}
-
-if (document.getElementById("fcamp").value="WinterCamp"){
-  if (document.getElementById("fjahr").value <2018){
+  if (document.getElementById("fjahr").value < 2016 || document.getElementById("fjahr").value > heute.getFullYear()){
     fehler += "Ungültiges Jahr eingegeben\n";
   }
 }
 
 if (document.getElementById("fcamp").value="WinterCamp"){
-  if (document.getElementById("fjahr").value >heute.getFullYear()){
-    fehler+= "Ungültiges Jahr eingegeben\n"
- }
+  if (document.getElementById("fjahr").value < 2018 || document.getElementById("fjahr").value > heute.getFullYear()){
+    fehler += "Ungültiges Jahr eingegeben\n";
+  }
 }
 
 //fehler Sonderzeichen ausgabe
-if (isValid(document.getElementById("fbenutzername").value)){
-  fehler+="Folgende Sonderzeichen können nicht benutzt werden: [~`$%\^*+=\-\[\]\\';/{}|\\<>]\n"
-}
-
-if (isValid(document.getElementById("fvorname").value)){
-  fehler+="Folgende Sonderzeichen können nicht benutzt werden: [~`$%\^*+=\-\[\]\\';/{}|\\<>]\n"
-}
-
-if (isValid(document.getElementById("fnachname").value)){
-  fehler+="Folgende Sonderzeichen können nicht benutzt werden: [~`$%\^*+=\-\[\]\\';/{}|\\<>]\n"
-}
-
-if (isValid(document.getElementById("fjahr").value)){
-  fehler+="Folgende Sonderzeichen können nicht benutzt werden: [~`$%\^*+=\-\[\]\\';/{}|\\<>]\n"
-}
-
-if (isValid(document.getElementById("fcamp").value)){
-  fehler+="Folgende Sonderzeichen können nicht benutzt werden: [~`$%\^*+=\-\[\]\\';/{}|\\<>]\n"
-}
-
-
-
-if (isValid(document.getElementById("fpasswort1").value)){
-  fehler+="Folgende Sonderzeichen können nicht benutzt werden: [~`$%\^*+=\-\[\]\\';/{}|\\<>]\n"
-}
-
-if (isValid(document.getElementById("fpasswort2").value)){
-  fehler+="Folgende Sonderzeichen können nicht benutzt werden: [~`$%\^*+=\-\[\]\\';/{}|\\<>]\n"
+if (isValid(document.getElementById("fvorname").value) && isValid(document.getElementById("fnachname").value) && isValid(document.getElementById("fbenutzername").value)){
+  fehler+="Folgende Sonderzeichen können nicht benutzt werden: [~`$%\^*+=\-\[\]\\';/{}|\\<>] bei Vorname, Nachname und Benutzername\n"
 }
 
 //datenschutz
@@ -96,17 +49,16 @@ if (fehler != ""){
 var fehlertext = "Die folgenden Felder wurden nicht vollständig ausgefüllt:\n\n";
 fehlertext += fehler;
 alert(fehlertext + "\nBitte füll die Informationen noch aus. Danke.");
-
 }
+}   
 
-}
 //teilnehmer
 function sichtbarkeit(){
  if(document.getElementById("fteilnehmer").checked == true){
    var elem1=document.getElementById("teilnehmerjahr");
    elem1.style.visibility = 'visible';
  }
- else{
+ else {
    var elem1=document.getElementById("teilnehmerjahr");
    elem1.style.visibility = 'hidden';
  }
